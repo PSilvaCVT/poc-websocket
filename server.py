bb17f387cbdd758d55d72e9beadd0cde3de7e986
Pedro Silva <ps@cvt.services>
2023-02-22 22:46:15 +0000

    - clear server.py

--- server.py
+++ server.py
@@ -15,24 +15,73 @@ CONNECTIONS = {}
 ENV = os.environ.get("WS_ENV")
 
 
+async def broadcast(chat_id: str, message: str):
+    if chat_id not in CONNECTIONS.keys():
+        return
+    for websocket in CONNECTIONS[chat_id].copy():
+        try:
+            await websocket.send(message)
+        except websockets.ConnectionClosed:
+            pass
+
+
+async def propagate(websocket, chat_id: str, message: str):
+    if chat_id not in CONNECTIONS.keys():
+        return
+    other_websockets = list(filter(
+        lambda ws_it: ws_it.id != websocket.id,
+        CONNECTIONS[chat_id]
+    ))
+    for ws in other_websockets:
+        try:
+            await ws.send(message)
+        except websockets.ConnectionClosed:
+            pass
+
+
+async def process_message_events():
+    print("creating redis connection:")
+    redis = aioredis.from_url(
+        os.environ.get("REDIS_URL"),
+        password=os.environ.get("REDIS_PASSWORD"),
+    )
+    print("Created!")
+    print("Subscribing to redis message_event...")
+    pubsub = redis.pubsub()
+    await pubsub.subscribe("message_event")
+    print("Subscribed!")
+    async for message in pubsub.listen():
+        if message["type"] != "message":
+            continue
+        payload = message["data"].decode()
+        message_obj = json.loads(payload)
+        chat_id = message_obj["chat_id"]
+        message_data = json.dumps(message_obj["data"])
+        print("received message from redis:", message_data)
+        await broadcast(chat_id=chat_id, message=message_data)
+    print("Redis Subscription connection closed.")
+
+
 def _register_websocket(websocket, key: str):
     if key not in CONNECTIONS.keys():
         CONNECTIONS[key] = []
     if websocket not in CONNECTIONS[key]:
-        print(f"connected id={websocket.id} chat_id={key}")
+        print(f"Connected id={websocket.id} chat_id={key}")
         CONNECTIONS[key].append(websocket)
 
 
 def _unregister_websocket(websocket, key: str):
     if CONNECTIONS[key]:
-        print(f"disconnected id={websocket.id} chat_id={key}")
+        print(f"Disconnected id={websocket.id} chat_id={key}")
         CONNECTIONS[key].remove(websocket)
 
 
 async def register_handler(websocket, chat_id: str):
     _register_websocket(websocket, key=chat_id)
     try:
-        await websocket.wait_closed()
+        async for message in websocket:
+            print(f"{websocket.id}-{chat_id}: {message}")
+            await broadcast(chat_id=chat_id, message=message)
     finally:
         _unregister_websocket(websocket, key=chat_id)
 
@@ -59,6 +108,7 @@ def get_server():
 
 async def main():
     async with get_server():
-        await asyncio.Future()
+        await process_message_events()
+        # await asyncio.Future()
 
 asyncio.run(main())
